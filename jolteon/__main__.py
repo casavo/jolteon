825ef04d566a280237fed41dc523ae0d583726bd
Alex Ceccotti <alexceccotti5995@gmail.com>
2023-02-22 09:57:37 +0200

    feat: get pg connection params from env

--- jolteon/__main__.py
+++ jolteon/__main__.py
@@ -3,48 +3,23 @@ import yaml
 
 from jolteon import Config
 from jolteon.modules import Updater, print_query_ids
-from jolteon.utils import get_pg_connection
+from jolteon.utils import ConnectionParams, get_pg_connection
 
 app = typer.Typer()
 
 
 @app.command()
-def get_ids(
-    pg_host: str = "localhost",
-    pg_port: int = 5432,
-    pg_database: str = "postgres",
-    pg_user: str = "postgres",
-    pg_password: str = "postgres",
-) -> None:
-    conn = get_pg_connection(
-        host=pg_host,
-        port=pg_port,
-        database=pg_database,
-        user=pg_user,
-        password=pg_password,
-    )
+def get_ids(connection_params_from_env: bool = False) -> None:
+    conn = get_pg_connection(ConnectionParams(params_from_env=connection_params_from_env))
     print_query_ids(conn)
 
 
 @app.command()
-def update_db(
-    config_path: str,
-    pg_host: str = "localhost",
-    pg_port: int = 5432,
-    pg_database: str = "postgres",
-    pg_user: str = "postgres",
-    pg_password: str = "postgres",
-) -> None:
+def update_db(config_path: str, connection_params_from_env: bool = False) -> None:
     with open(config_path) as f:
         config = Config(**yaml.safe_load(f))
 
-    conn = get_pg_connection(
-        host=pg_host,
-        port=pg_port,
-        database=pg_database,
-        user=pg_user,
-        password=pg_password,
-    )
+    conn = get_pg_connection(ConnectionParams(params_from_env=connection_params_from_env))
     updater = Updater(config, conn)
     updater.overwrite_db()
 
--- jolteon/utils.py
+++ jolteon/utils.py
@@ -1,20 +1,36 @@
+import os
+
 import pandas as pd
 import psycopg2
+from pydantic import BaseModel, root_validator
+
+
+class ConnectionParams(BaseModel):
+    params_from_env: bool = False
+    host: str = "localhost"
+    port: int = 5432
+    database: str = "postgres"
+    user: str = "postgres"
+    password: str = "postgres"
+
+    @root_validator(pre=True)
+    def populate_params(cls, values: dict) -> dict:
+        if values["params_from_env"]:
+            for k, v in cls.schema()["properties"].items():
+                if k != "params_from_env":
+                    values[k] = os.getenv(f"PG_{k.upper()}", v["default"])
+        return values
 
 
 def get_pg_connection(
-    host: str,
-    port: int,
-    database: str,
-    user: str,
-    password: str,
+    connection_params: ConnectionParams,
 ) -> psycopg2.extensions.connection:
     return psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=user,
-        password=password,
+        host=connection_params.host,
+        port=connection_params.port,
+        database=connection_params.database,
+        user=connection_params.user,
+        password=connection_params.password,
     )
 
 
